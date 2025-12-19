10dce1a727e738cbe87a20925ddc384e0c925c80
emilyho14 <eeh2003@sbcglobal.net>
2025-12-19 17:18:57 -0600

    added photos of me

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from './pages/About.jsx'
 import Research from './pages/Research.jsx'
 import Secrets from './pages/Secrets.jsx'
 import Projects from "./pages/Projects.jsx";
+import Home from "./pages/Home.jsx";
 
 export default function App() {
   return (
@@ -17,16 +18,15 @@ export default function App() {
         <Link to="/Projects">Projects</Link>
         <Link to="/About" element={<About />}>About Me</Link>
         <Link to="/Contact">Contact</Link>
-         <Link to="/Secrets">Shh...</Link>
       </nav>
       </div>
 
       <div className="routes">
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/About" element={<About />} />
           <Route path="/Research" element={<Research />} />
           <Route path="/Projects" element={<Projects />} />
-          <Route path="/Secrets" element={<Secrets />} />
         </Routes>
       </div>
     </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+import me1 from "../../images/me/1.jpeg";
+import me2 from "../../images/me/2.jpeg";
+import me3 from "../../images/me/3.JPG";
+import me4 from "../../images/me/4.JPG";
+import me5 from "../../images/me/5.JPG";
+import me6 from "../../images/me/6.jpeg";
+import me7 from "../../images/me/7.JPG";
+import me8 from "../../images/me/8.JPG";
+import me9 from "../../images/me/9.JPG";
+
+export default function Home() {
+  const images = [me1, me2, me3, me4, me5, me6, me7, me8, me9];
+  const [i, setI] = useState(0);
+  const [paused, setPaused] = useState(false);
+
+  useEffect(() => {
+    if (paused) return;
+    const id = setInterval(
+      () => setI((p) => (p + 1) % images.length),
+      2000
+    );
+    return () => clearInterval(id);
+  }, [paused, images.length]);
+
+  return (
+    <div
+        className="home-me-photos"
+        style={{ "--bg": `url(${images[i]})` }}
+        onMouseEnter={() => setPaused(true)}
+        onMouseLeave={() => setPaused(false)}
+    >
+        <img src={images[i]} alt="" className="home-photos" />
+    </div>
+    );
+
+}
