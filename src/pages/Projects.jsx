4824ed8a1623aef606fa06ec9851945929f1ea9d
Emily Ho <emilyho@chicken-tender.local>
2026-05-10 02:31:41 -0500

    add new project to page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,6 +4,7 @@ export default function Projects() {
     return (
         <div className="personal-projects"> 
             <h2> Here's some of my for-fun personal projects! </h2>
+            <a href="https://photobooth.emilyho.dev/"> Photobooth at Home!! </a>
             <a href="https://emilyho14.github.io/tic-tac-toe/"> DYNAMIC Tic-Tac-Toe </a>
             <a href="https://emilyho14.github.io/Productivity-Tool"> My Meme Website </a>
         </div>
